64d7068795864a23787ea17293a5e6ea3f947ec8
lrl-tomato <46903132+lrl-tomato@users.noreply.github.com>
2019-02-14 14:22:28 -0500

    Update mloglikelihood.R

--- Rcode/mloglikelihood.R
+++ Rcode/mloglikelihood.R
@@ -1,7 +1,7 @@
-################################################################################
+#----------------------------------------------------------
 # For a Normal random sample
 # f(x) = 1/(sqrt(2*pi)*(sigma^2))*exp(-(x-mu)^2/2*sigma^2)
-################################################################################
+#----------------------------------------------------------
 
 mloglike_norm = function(theta,x){ # theta is (mu,sigmasq)
   mu = theta[1]; sigmasq = theta[2]
@@ -10,11 +10,10 @@ mloglike_norm = function(theta,x){ # theta is (mu,sigmasq)
   return(value)
 }
 
-################################################################################
+#----------------------------------------------------------
 # For a 2 parameters Weibull distribution
 # f(x|alpha, lambda) = alpha*lambda^(alpha)*exp(-(lambda*x)^alpha)), t >= 0
-################################################################################
-
+#----------------------------------------------------------
 mloglike_weibull = function(theta,x){ #theta is (alpha,lambda)
   alpha = theta[1]; lambda = theta[2]
   n = length(x)
@@ -22,10 +21,10 @@ mloglike_weibull = function(theta,x){ #theta is (alpha,lambda)
   return(value)
 }
 
-################################################################################
+#----------------------------------------------------------
 # Finding maximum likelihood estimate numerically
 # 2  S T E P S
-################################################################################
+#----------------------------------------------------------
 # help(optim) # Minimizing a function (mloglike functions stand for minus loglikehihood functions)
 
 startvals = c(,) # add-on e.g. can try using c(0,1) for mloglike_norm
